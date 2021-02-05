822cb5d4f9800f739289b0ba12271bdc0618fc67
Trinh Le Hai Nam <trinhlehainamjapan@gmail.com>
2021-02-05 13:33:56 +0900

    Fixed volumetric scatter position and checking range
    - Checking intersect range.
    - Scatter ray or hit position is at generated distance created from density parameter.

--- RaytracingStudy/Objects/ConstantMedium.cpp
+++ RaytracingStudy/Objects/ConstantMedium.cpp
@@ -24,38 +24,48 @@ bool ConstantMedium::IsHit(const Ray& ray, float rangeMin, float rangeMax, HitRe
     const bool enableDebug = false;
     const bool debugging = enableDebug && MathHelper::Random<float>() < 0.00001f;
 
-    HitRecord record_1, record_2;
+    // Record when the ray first intersect with the volume
+    // And record when the ray go through the volume and intersect other side of volume
+    HitRecord record_min, record_max;
 
-    if (!Boundary->IsHit(ray, -MathHelper::INFINITY_FLOAT, MathHelper::INFINITY_FLOAT, record_1))
+    //
+    /* Checking volume by letting the LINE contents ray go through the object */
+    //
+    if (!Boundary->IsHit(ray, -MathHelper::INFINITY_FLOAT, MathHelper::INFINITY_FLOAT, record_min))
         return false;
-    if (!Boundary->IsHit(ray, record_1.t + 0.001f, MathHelper::INFINITY_FLOAT, record_2))
+    if (!Boundary->IsHit(ray, record_min.t + 0.001f, MathHelper::INFINITY_FLOAT, record_max))
         return false;
+    /*------------------------------------------------------------------------*/
 
     if (debugging)
-        std::cerr << "\n t_min = " << record_1.t << "\n t_max = " << record_2.t << "\n";
+        std::cerr << "\n t_min = " << record_min.t << "\n t_max = " << record_max.t << "\n";
 
-    record_1.t = MathHelper::Max(record_1.t, rangeMin);
-    record_2.t = MathHelper::Min(record_2.t, rangeMax);
+    record_min.t = record_min.t < rangeMin ? rangeMin : record_min.t;
+    record_max.t = record_max.t > rangeMax ? rangeMax : record_max.t;
 
-    if (record_1.t < 0.0f) record_1.t = 0.0f;
+    if (record_min.t > record_max.t) return false;
+
+    if (record_min.t < 0.0f) record_min.t = 0.0f;
 
     const float ray_length = ray.Direction.Length();
-    const float travel_distance = (record_2.t - record_1.t) * ray_length;
+    const float travel_distance = (record_max.t - record_min.t) * ray_length;
     const float hit_distance = Inv_Neg_Density * std::logf(MathHelper::Random<float>());
 
+    // Hit only when generated distance (hit_distance) is inside the volume
     if (hit_distance > travel_distance) return false;
 
-    record.t = record_1.t + travel_distance / ray_length;
+    record.t = record_min.t + hit_distance / ray_length;
     record.Position = ray.GetPositionFromParameter(record.t);
+    record.pMaterial = PhaseFunction;
 
     if (debugging)
         std::cerr << "\n hit_distance = " << hit_distance <<
         "\n record.t = " << record.t << "\n";
 
+    // Abitrary, it doesn't effect the scattered ray from Isotropic Material
     record.Normal = Vector3D(1.0f, 0.0f, 0.0f);
     record.FrontFace = true;
-    record.pMaterial = PhaseFunction;
-
+    
     return true;
 }
 
